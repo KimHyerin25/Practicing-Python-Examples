946ec9a3250865bdccd4a29c8097a86cefcc34d4
SoYeon Yang <85726134+xoyeon@users.noreply.github.com>
2021-12-17 20:12:40 +0900

    211214

--- 로또의 최고 순위와 최저 순위.py
+++ 로또의 최고 순위와 최저 순위.py
@@ -1,3 +1,24 @@
+# 망한 내 답
+def solution(lottos, win_nums):
+    answer = []
+    if lottos.count(0) = 0:
+        answer = [1,1]
+    if lottos.count(0) = 1:
+        answer = [1,2]
+    if lottos.count(0) = 2:
+        answer = [3,5]
+    if lottos.count(0) = 3:
+        answer = [4,4]
+    if lottos.count(0) = 4:
+        answer = [3,5]
+    if lottos.count(0) = 5:
+        answer = [1,6]
+    if lottos.count(0) = 6:
+        answer = [1,6]
+        
+    return answer
+
+# 예시답안
 def solution(lottos, win_nums):
     rank = [6,6,5,4,3,2,1]
     cnt = 0
