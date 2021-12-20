1616caecc817cd037b4a9f364f0f590d5a167861
SoYeon Yang <85726134+xoyeon@users.noreply.github.com>
2021-12-20 10:49:04 +0900

    211219

--- 숫자 문자열과 영단어.py
+++ 숫자 문자열과 영단어.py
@@ -1,3 +1,11 @@
 def solution(s):
-    answer = 0
-    return answer
+    answer = s
+    
+    words = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4',
+            'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    
+    for key, value in words.items():
+        answer = answer.replace(key, value)
+        
+    
+    return int(answer)
